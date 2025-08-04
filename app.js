0596bbb515615c12ea9318757783bae9890ffeef
personal <obryanno@msu.edu>
2025-08-04 12:05:09 -0400

    added a swagger page for easy api access

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const app = express();
 app.use(express.json());
 
 const bookmarkRoutes = require('./routes/bookmarks')
+const swagger = require('./swagger')
+
+// Swagger served
+app.use('/api-docs', swagger.serve, swagger.setup)
+
+// Routes
+app.use('/bookmarks', bookmarkRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Bookmark Manager API is running');
+});
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -14,13 +25,8 @@ mongoose.connect(process.env.MONGODB_URI, {
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
 
-app.get('/', (req, res) => {
-  res.send('Bookmark Manager API is running');
-});
-
-app.use('/bookmarks', bookmarkRoutes);
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
 });
